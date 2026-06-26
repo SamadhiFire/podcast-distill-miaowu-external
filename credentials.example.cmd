9abfe7a8123f550da5a6d03782512f386fd9fbc2
SamadhiFire <chenyang711711@gmail.com>
2026-06-26 22:58:21 +0800

    2258

---
+++ credentials.example.cmd
@@ -0,0 +1,5 @@
+@REM 复制为 credentials.cmd 并填入真实值，该文件已加入 .gitignore
+set FEISHU_APP_ID=your_app_id
+set FEISHU_APP_SECRET=your_app_secret
+set FEISHU_WIKI_SPACE_ID=your_wiki_space_id
+set FEISHU_NOTIFY_WEBHOOK=https://open.feishu.cn/open-apis/bot/v2/hook/xxx
